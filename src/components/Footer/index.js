9085d3381d2ca59a7c4e40e02d10f292056274e7
ArlonTuazon <nikka1378@yahoo.com>
2021-03-16 21:07:37 -0700

    edit files

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ function Footer() {
         <a href="https://www.linkedin.com/in/maria-arlonnica-tuazon-72b84550"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" class="icon"/></a>
         
               
-        &copy; arlonTuazon  |  <a href="https://github.com/arlontuazom" class="link">download resume</a>  |  <a href="https://coolors.co/f06543-e8e9eb-e0dfd5-313638-f09d51">color palette</a>  |  made with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/>
+        &copy; arlonTuazon  |  <a href="https://github.com/arlontuazom" class="link">download resume</a>  |   <a href="https://icons8.com/icon/uEPeFRqfMJL6/download">Download icon by Icons8</a>  |  made with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/>
        
 
     </div>
